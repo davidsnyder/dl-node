5b3572f7df15386ce98e065bbd42f69ff4e354ee
David Snyder <david@infochimps.org>
2011-12-09 17:32:01 -0600

    trying to get scoping by vote session to work

--- app.js
+++ app.js
@@ -1,45 +1,57 @@
 var channel_name = 'dl.channel.votes';
 
 const redis = require('redis');
-const sub   = redis.createClient();
-const store = redis.createClient();
+const redis_store = redis.createClient();
 
-const io     = require('socket.io');
-const socket = io.listen(3000);
+const io = require('socket.io').listen(3000);
 
-socket.sockets.on('connection', function(client) {
+io.sockets.on('connection', function(client) {
 
+    const sub = redis.createClient(); //create a new subscriber connection
+    
     sub.subscribe(channel_name);  //listen for messages published on this channel
 
-    sub.on("message", function(channel, vote) { //receives "meal_id:user_id:restaurant_id"
+    client.on('join', function(meal_id) {
+        client.set('meal_id', meal_id);
+        client.join(meal_id);
+        console.log("user joined /"+meal_id);
+    })
+
+    sub.on("message", function(channel, vote) { //receives "meal_id:user_id:restaurant_id" messages from redis
         var vote_keys = vote.split(":");
         var key = vote_keys[0]+":"+vote_keys[1];
-        store.set(key,vote_keys[2]); //register this vote in the db
+        redis_store.set(key,vote_keys[2]); //register this vote in the db
 
-        var meal = {'meal_id':vote_keys[0],'total':0,'votes':{}};
-        
-        store.keys(vote_keys[0]+":*", function (err, votes) { //grab all votes for this meal
-            meal['total'] = votes.length;
-            votes.forEach(function (vote) {  //assemble response payload 
-                store.get(vote,function(err,rest_id) {
-                    if(meal['votes'][rest_id] == undefined) {
-                        meal['votes'][rest_id] = [];           
-                    }                                              
-                    meal['votes'][rest_id].push(vote.split(':')[1]);           
+        client.get('meal_id',function(err,vote_session) { //we only want to send updates to clients in this vote session
+            var meal = {'meal_id':vote_session,'total':0,'votes':{'carlos':'fix me'}};
+            redis_store.keys(vote_keys[0]+":*", function (err, votes) { //grab all votes for this meal
+                meal['total'] = votes.length;
+                votes.forEach(function (vote) {  //assemble response payload 
+                    redis_store.get(vote,function(err,rest_id) {
+                        if(meal['votes'][rest_id] == undefined) {
+                            meal['votes'][rest_id] = [];           
+                        }                                              
+                        meal['votes'][rest_id].push(vote.split(':')[1]);           
+                    });
                 });
             });
+            
+            //How do I get the changes made in the function closures above to reflect in @meal@?
+            io.sockets.in(vote_session).emit('vote', JSON.stringify(meal));             
         });
-
-        //How do I get the changes made in the function closures to reflect here?
-        console.log(meal);
-        client.emit('vote',JSON.stringify(meal));
     });
 
     client.on("connect",function() {
-        client.emit('message',"Connected");
+        console.log("user connected");        
     });
 
     client.on('disconnect', function() {
-        client.emit('message',"Disconnected");
+        client.get('meal_id',function(err,vote_session) {
+            client.leave("/"+vote_session);
+            sub.quit();
+            console.log(io.rooms);
+            console.log("user left /"+vote_session);
+        });
     });
+    
 });                   
