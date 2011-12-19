ab8bb44d4855ab800a17fb785435e416988782f3
David Snyder <david@infochimps.org>
2011-12-18 23:56:11 -0600

    making node app as dumb as possible

--- app.js
+++ app.js
@@ -2,56 +2,27 @@ var channel_name = 'dl.channel.votes';
 
 const redis = require('redis');
 const redis_store = redis.createClient();
-
 const io = require('socket.io').listen(3000);
 
 io.sockets.on('connection', function(client) {
 
     const sub = redis.createClient(); //create a new subscriber connection
-    
     sub.subscribe(channel_name);  //listen for messages published on this channel
 
-    client.on('join', function(vote_session) {
-        client.set('vote_session', vote_session);
-        client.join(vote_session);
-        emitSessionResponse(vote_session); //new connections get the current session state on join
-        console.log("user joined /"+vote_session);
+    client.on('join', function(session_id) {
+        client.set('vote_session', session_id);
+        client.join(session_id);
+        console.log("user joined /"+session_id);
     });
 
-    sub.on("message", function(channel, vote_json) {
-        var obj = JSON.parse(vote_json);
-        var vote = obj.vote;
-        redis_store.set(obj.session_id+":"+vote.user.id,JSON.stringify(vote)); //register this vote in the db
+    sub.on("message", function(channel, session) {
+        var session_id = JSON.parse(session).uuid; //this is a waste, there's probably a way to avoid it
         client.get('vote_session',function(err,vote_session) { //we only want to send updates to clients in this vote session
-            if(vote_session == obj.session_id) {
-                emitSessionResponse(obj.session_id);
+            if(vote_session == session_id) {
+              client.emit('vote', session);              
             }
         });
     });
-
-    function emitSessionResponse(session_id) {
-        var session = {
-            "session_id": session_id,
-            "total_votes":0,
-            "options": {}
-        };       
-        redis_store.keys(session_id+":*", function(err, votes) { //grab all votes for this session_id
-            session['total_votes'] = votes.length;
-            votes.forEach(function(vote_id) {  //assemble response payload
-                redis_store.get(vote_id,function(err,vote_json) {
-                    var vote = JSON.parse(vote_json);
-                    if(session.options[vote.option.id] == undefined) {
-                        session.options[vote.option.id] = {'id':vote.option.id,'name':vote.option.name,'votes':[]};           
-                    }                                              
-                    session.options[vote.option.id].votes.push(vote.user);
-                    if(votes[votes.length-1] == vote_id) { //hackity-hack (don't talk back, workaround for async issue)
-                        client.emit('vote', JSON.stringify(session));                             
-                    }
-                });
-            });
-        });
-        return session;
-    }
 
     client.on('disconnect', function() {
         client.get('vote_session',function(err,vote_session) {
