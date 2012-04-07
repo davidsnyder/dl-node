aaf9e69eaf69308ef96652cacd5f90c73da5e49e
David Snyder <dsnyder90@gmail.com>
2012-04-07 17:22:32 -0500

    cleaning up formatting

--- app.js
+++ app.js
@@ -25,14 +25,14 @@ io.sockets.on('connection', function(client) {
     sub.subscribe(channel_name);  //listen for messages published on this channel
 
     client.on('join', function(session_id) {
-        client.set('vote_session', session_id);
+        client.set('vote_session',session_id);
         client.join(session_id);
         //console.log("user joined /"+session_id);
     });
 
-    sub.on("message", function(channel, session) {
+    sub.on("message", function(channel,session) {
         var session_id = JSON.parse(session).uuid; //this is a waste, there's probably a way to avoid it
-        client.get('vote_session',function(err,vote_session) { //we only want to send updates to clients in this vote session
+        client.get('vote_session', function(err,vote_session) { //we only want to send updates to clients in this vote session
             if(vote_session == session_id) {
               client.emit('vote', session);              
             }
@@ -40,8 +40,8 @@ io.sockets.on('connection', function(client) {
     });
 
     client.on('disconnect', function() {
-        client.get('vote_session',function(err,vote_session) {
-            client.leave("/"+vote_session);
+        client.get('vote_session', function(err,vote_session) {
+            client.leave("/" + vote_session);
             sub.quit();
             //console.log("user left /"+vote_session);                        
         });
